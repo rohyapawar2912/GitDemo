e91f85fd40854e2e33d017cfdeb171cf48e501b9
“rohyapawar2912” <rohitpawar8530@gmail.com>
2023-06-14 09:31:45 +0530

    [Rohit]Add: // add files

---
+++ GitDemo/GitDemoClass1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitDemo
+{
+    public class GitDemoClass1 
+    { 
+        public void Demo ()
+        {
+            Console.WriteLine("Hi All" );
+        } 
+    }
+
+
+}
+
+
+
+
